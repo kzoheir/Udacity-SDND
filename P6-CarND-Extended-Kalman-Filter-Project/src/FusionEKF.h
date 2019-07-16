df4c7e084dac3ccdacf1623c9c3ad3f831135f50
Khaled Zoheir <khaled.zoheir.kz@gmail.com>
2019-07-16 22:31:45 +0200

    Add files via upload

---
+++ P6-CarND-Extended-Kalman-Filter-Project/src/FusionEKF.h
@@ -0,0 +1,59 @@
+#ifndef FusionEKF_H_
+#define FusionEKF_H_
+
+#include "measurement_package.h"
+#include "Eigen/Dense"
+#include <vector>
+#include <string>
+#include <fstream>
+#include "kalman_filter.h"
+#include "tools.h"
+
+class FusionEKF {
+public:
+  /**
+  * Constructor.
+  */
+  FusionEKF();
+
+  /**
+  * Destructor.
+  */
+  virtual ~FusionEKF();
+
+  /**
+  * Run the whole flow of the Kalman Filter from here.
+  */
+  void ProcessMeasurement(const MeasurementPackage &measurement_pack);
+
+  /**
+  * Kalman Filter update and prediction math lives in here.
+  */
+  KalmanFilter ekf_;
+
+private:
+  // check whether the tracking toolbox was initialized or not (first measurement)
+  bool is_initialized_;
+
+  // previous timestamp
+  long long previous_timestamp_;
+
+  // tool object used to compute Jacobian and RMSE
+  Tools tools;
+  Eigen::MatrixXd R_laser_;
+  Eigen::MatrixXd R_radar_;
+  Eigen::MatrixXd H_laser_;
+  Eigen::MatrixXd Hj_;
+
+	//acceleration noise components
+  float noise_ax = 9.0;
+  float noise_ay = 9.0;
+
+	//laser position measurment noise components
+  float noise_laser_px = 0.15;
+  float noise_laser_py = 0.15;
+  float noise_laser_px_sqr = noise_laser_px * noise_laser_px;
+  float noise_laser_py_sqr = noise_laser_py * noise_laser_py;
+};
+
+#endif /* FusionEKF_H_ */
